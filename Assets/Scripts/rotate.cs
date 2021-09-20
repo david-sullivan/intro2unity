07f094f6abce27b47776c3db2a409e962fc282fa
david <david@swampmonster.org>
2021-09-19 22:27:15 -0500

    initial commit

---
+++ Assets/Scripts/rotate.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotate : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the object around its local X axis at 1 degree per second
+        transform.Rotate(Vector3.right * Time.deltaTime);
+
+        // ...also rotate around the World's Y axis
+        transform.Rotate(Vector3.up * Time.deltaTime, Space.World);
+    }
+}
