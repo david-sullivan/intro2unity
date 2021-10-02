26e3f93b3bae3c8e21e7d6e7d02d3e8f2a601941
david <david@swampmonster.org>
2021-10-02 18:24:56 -0500

    added spawnManager to falling

---
+++ Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);// destroy the object
+        Destroy(other.gameObject); // destroy the object it collided with
+    }
+
+}
--- Assets/Scripts/Fall_SpawnManager.cs
+++ Assets/Scripts/Fall_SpawnManager.cs
@@ -7,13 +7,15 @@ public class Fall_SpawnManager : MonoBehaviour
 {
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     public GameObject[] fallingObjectsPrefabs;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomObject", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
@@ -21,9 +23,14 @@ public class Fall_SpawnManager : MonoBehaviour
     {
         if (Keyboard.current.sKey.wasPressedThisFrame)
         {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int objectIndex = Random.Range(0, fallingObjectsPrefabs.Length);
-            Instantiate(fallingObjectsPrefabs[objectIndex], spawnPos, fallingObjectsPrefabs[objectIndex].transform.rotation);
+            
         }
+    }
+
+    void SpawnRandomObject()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int objectIndex = Random.Range(0, fallingObjectsPrefabs.Length);
+        Instantiate(fallingObjectsPrefabs[objectIndex], spawnPos, fallingObjectsPrefabs[objectIndex].transform.rotation);
     }
 }
